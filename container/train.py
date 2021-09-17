b1579327ba8c8c61c1cad87fcb675120c60b18b1
David Hundley <david.k.hundley@gmail.com>
2021-09-17 13:41:49 -0500

    Starting the formal model train files

---
+++ container/train.py
@@ -0,0 +1,33 @@
+# Importing the necessary Python libraries
+import os
+import cloudpickle
+import pandas as pd
+from category_encoders.one_hot import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import FunctionTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+
+import warnings
+warnings.filterwarnings('ignore')
+
+
+
+## DIRECTORY INSTANTIATION
+## ---------------------------------------------------------------------------------------------------------------------
+# Defining how SageMaker
+prefix = '/opt/ml/'
+input_path = os.path.join(prefix, 'input/data')
+output_path = os.path.join(prefix, 'output')
+model_path = os.path.join(prefix, 'model')
+
+
+
+## HELPER FUNCTIONS
+## ---------------------------------------------------------------------------------------------------------------------
+
+
+
+## MODEL TRAIN SCRIPT
+## ---------------------------------------------------------------------------------------------------------------------
